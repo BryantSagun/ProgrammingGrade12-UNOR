d10ab466f50b061ac357a950682545f11b15d358
Bryant Kyle Sagun <bryantkyle.sagun@gmail.com>
2020-02-22 21:56:41 +0800

    Update TypesOfTriangles.py

--- TypesOfTriangles.py
+++ TypesOfTriangles.py
@@ -1,11 +1,20 @@
+run = True
 print("Identifying Types of Triangle.")
-sideA = float(input("Enter measurement for Side A (cm): "))
-sideB = float(input("Enter measurement for Side B (cm): "))
-sideC = float(input("Enter measurement for Side B (cm): "))
+while run:
+    sideA = float(input("Enter measurement for Side A (cm): "))
+    sideB = float(input("Enter measurement for Side B (cm): "))
+    sideC = float(input("Enter measurement for Side B (cm): "))
 
-if sideA == sideB and sideB == sideC:
-    print("The Triangle is an Equilateral Triangle.")
-elif sideA == sideB or sideB == sideC or sideA == sideC:
-    print("The Triangle is an Isosceles Triangle.")
-else:
-    print("The Triangle is a Scalene Triangle.")
+    if sideA == sideB and sideB == sideC:
+        print("The Triangle is an Equilateral Triangle.")
+    elif sideA == sideB or sideB == sideC or sideA == sideC:
+        print("The Triangle is an Isosceles Triangle.")
+    else:
+        print("The Triangle is a Scalene Triangle.")
+
+    runAgain = int(input("Run the program again? [1] Yes [2] No: "))
+    if runAgain == 1:
+        run = True
+    else:
+        run = False
+print("Thank you for running the program")
